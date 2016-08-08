92b91940bd3c7976e239f79b914bf498638b9548
Apple <Apple@apples-macbook.local>
2016-08-08 14:43:06 +0530

    Initial Commit

---
+++ HairApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HairApp
+//
+//  Created by Apple on 08/08/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
